d15a00ae74ddc00cea1dff04b97c7920b2e88ab5
Wisterleys <wister.sousa2@gmail.com>
2023-05-05 10:07:21 -0300

    feat: Configuração inicial do projeto laravel realizada e com seed para usuário especifico criado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+         \App\Models\User::create([
+            'name' => 'Wister',
+            'email' => 'wister.sousa2@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin'),
+            'remember_token' => Str::random(10),
+
+        ]);
+        
     }
 }
